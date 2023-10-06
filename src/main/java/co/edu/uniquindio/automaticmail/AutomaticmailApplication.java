517148befe7e70d22555738d9ccc65a0135a067b
David Serna Restrepo <david.sernar@uqvirtual.edu.co>
2023-10-05 21:00:42 -0500

    Creación y Configuración del Proyecto

---
+++ src/main/java/co/edu/uniquindio/automaticmail/AutomaticmailApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.automaticmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticmailApplication.class, args);
+    }
+
+}
